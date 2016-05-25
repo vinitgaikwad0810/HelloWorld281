009d06af8581a32d403af51d72e3cb0c2ddd7853
vinitgaikwad0810 <vinit.gaikwad10@gmail.com>
2016-05-25 10:56:29 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+/*
 app.get('/', function(request, response) {
   response.render('pages/index')
-});
+});*/
 
+/*
 app.get('/cool', function(request, response) {
   response.send(cool());
-});
+});*/
 
 
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+
+app.get('/', function(request, response) {
+ 
+ 	console.log("Hello World");
+  response.send("Hello World");
 });
 
 app.listen(app.get('port'), function() {
