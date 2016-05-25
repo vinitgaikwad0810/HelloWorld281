fd79a4025753942962ffd6baf457f1242052e7aa
vinitgaikwad0810 <vinit.gaikwad10@gmail.com>
2016-05-25 11:48:26 -0700

    dfsf

--- index.js
+++ index.js
@@ -19,14 +19,81 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });*/
 
+var DB = require('mongodb').Db,
+    DB_Connection = require('mongodb').Connection,
+    DB_Server = require('mongodb').Server,
+    async = require('async') ;
+
+var MongoClient = require('mongodb').MongoClient;
+ var url1 = "mongodb://52.24.136.109:27017/test";
+
+ var url2 = "mongodb://52.35.94.222:27017/test";
+ var url3 = "mongodb://52.33.102.9:27017/test";
 
 
-app.get('/', function(request, response) {
- 
- 	console.log("Hello World");
-  response.send("Hello World");
+
+app.get('/server2', function(request, response) {
+MongoClient.connect(url2, function(err, db) {
+db.collection('cmpe281', function(err, collection) {
+
+        collection.find( {key: "1"}).toArray( function(err, results) {
+
+        	  response.setHeader("Content-Type", "text/html");	
+              response.end("SERVER 2:"+results[0].value + "SERVER 1:"+results[0].value + "SERVER 3:"+results[0].value);
+           //   output += "SERVER 2:"+results[0].value;
+
+        } ) ;
+
+    
+});
+});
 });
 
+/*
+app.get('/server3', function(request, response) {
+MongoClient.connect(url3, function(err, db) {
+db.collection('cmpe281', function(err, collection) {
+
+        collection.find( {key: "1"}).toArray( function(err, results) {
+
+        	  response.setHeader("Content-Type", "text/html");	
+              response.end("SERVER 3:"+results[0].value);
+           //   output += "SERVER 2:"+results[0].value;
+
+        } ) ;
+
+    
+});
+});
+});
+*/
+/*
+app.get('/server1', function(request, response) {
+ 
+ 
+//var output = " ";
+  //response.send("Hello World");
+MongoClient.connect(url1, function(err, db) {
+db.collection('cmpe281', function(err, collection) {
+
+        collection.find( {key: "1"}).toArray( function(err, results) {
+
+        	  response.setHeader("Content-Type", "text/html");	
+              response.end("SERVER 1:"+results[0].value);
+
+              //output = "SERVER 1:"+results[0].value;
+
+        } ) ;
+
+    
+});
+});
+
+*/
+
+
+
+//
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
